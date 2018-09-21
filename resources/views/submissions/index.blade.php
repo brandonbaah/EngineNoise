98124dbd935d4e8976d29de2b6bba222dc611bdf
Brandon Baah <brandonbaah@gmail.com>
2018-09-21 14:08:51 -0400

    added new attributes to submissions

--- resources/views/submissions/index.blade.php
+++ resources/views/submissions/index.blade.php
@@ -13,30 +13,44 @@
                         <th>City</th>
                         <th class="text-right">List Price</th>
                         <th class="text-right">Offer Price</th>
-                        <th class="text-right">Percentage Below Market</th>
+                        <th class="text-right">PBM</th>
+                        <th class="text-right">Reno Cost</th>
+                        <th class="text-right">ARV</th>
                         </thead>
                         <tbody>
                         @foreach($submissions as $submission)
                             <tr>
                                 <td><a href="" class="link-blue">{{$submission->address}}</a></td>
-                                <td>brand</td>
+                                <td>{{$submission->city}}</td>
 
                                 <td class="text-right">
-                                    <span class="approvals one"><i class="fa fa-file-text"></i>
+                                    <span class=""><i class="fa fa-file-text"></i>
                                         &nbsp;
-                                        {{$submission->city}}
+                                        {{number_format($submission->list_price)}}
                                     </span>
                                 </td>
                                 <td class="text-right">
-                                    <span class="approvals two "><i class="fa fa-file-text"></i>
+                                    <span class=""><i class="fa fa-file-text"></i>
                                         &nbsp;
-                                       {{$submission->offer_price}}
+                                       {{number_format($submission->offer_price)}}
                                     </span>
                                 </td>
                                 <td class="text-right">
-                                    <span class="approvals three"><i class="fa fa-file-text"></i>
+                                    <span class=""><i class="fa fa-file-text"></i>
                                         &nbsp;
-                                       {{$submission->percent_below_market}}
+                                       {{strval(round($submission->percent_below_market)).'%'}}
+                                    </span>
+                                </td>
+                                  <td class="text-right">
+                                    <span class=""><i class="fa fa-file-text"></i>
+                                        &nbsp;
+                                       {{number_format($submission->repair_cost)}}
+                                    </span>
+                                </td>
+                                  <td class="text-right">
+                                    <span class=""><i class="fa fa-file-text"></i>
+                                        &nbsp;
+                                       {{number_format($submission->arv)}}
                                     </span>
                                 </td>
                             </tr>
